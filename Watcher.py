95785390ef1a68b97a94d5bda29eb20f6713ef8b
Jakub Zimny <jakub.zimny.gdansk@gmail.com>
2026-02-14 11:21:34 +0100

    refactor: allow FolderWatcher to pass config to event handler

--- Watcher.py
+++ Watcher.py
@@ -5,11 +5,11 @@ from watchdog.observers import Observer
 from handler import FileMoveHandler
 
 
-class Watcher:
+class FolderWatcher:
 
-    def __init__(self, path_to_watch: Path):
+    def __init__(self, path_to_watch: Path, directories_config: dict = None):
         self.path_to_watch = path_to_watch
-        self.handler = FileMoveHandler()  
+        self.handler = FileMoveHandler(directories_config)
         self.observer = Observer()
 
     def start(self):
